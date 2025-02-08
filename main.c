c2a45ef0542b7a2b476a19b568fab438ec55023a
maliby <nikitabaoy@gmail.com>
2025-02-08 10:21:23 +0300

    purupupu

--- main.c
+++ main.c
@@ -10,10 +10,6 @@ int main(int argc, char **argv)
     char first[255], last[255];
 >>>>>>> d2302028482a471cc19abbaae15fc899e7e89eff
 
-    printf("Enter your name: ");
-    fgets(name, 255, stdin);
-    printf("length = %d\n", strlen(name)); /* debug line */
-    name[strlen(name)-1] = '\0'; /* remove the newline at the end */
     printf("Enter your first name: ");
     fgets(first, 255, stdin);
     first[strlen(first)-1] = '\0'; /* remove the newline at the end */
